a1c4c9185d87e737d5d552e51207e7d6adcc378a
Dmitry Savransky <dsavransky@gmail.com>
2018-12-21 11:48:05 -0500

    upating differentiation function for sympy 1.3 and python 3 compatibility

--- utils/sympyhelpers.py
+++ utils/sympyhelpers.py
@@ -28,15 +28,15 @@ def difftotal(expr, diffby, diffmap):
         -theta_dot*sin(theta)
     """
     # Replace all symbols in the diffmap by a functional form
-    fnexpr = expr.subs({s:s(diffby) for s in diffmap})
+    fnexpr = expr.subs({s:Function(str(s))(diffby) for s in diffmap})
     # Do the differentiation
     diffexpr = diff(fnexpr, diffby)
     # Replace the Derivatives with the variables in diffmap
-    derivmap = {Derivative(v(diffby), diffby):dv 
-                for v,dv in diffmap.iteritems()}
+    derivmap = {Derivative(Function(str(v))(diffby), diffby):dv 
+                for v,dv in diffmap.items()}
     finaldiff = diffexpr.subs(derivmap)
     # Replace the functional forms with their original form
-    return finaldiff.subs({s(diffby):s for s in diffmap})
+    return finaldiff.subs({Function(str(s))(diffby):s for s in diffmap})
 
 def difftotalmat(mat,diffby,diffmap):
     """Apply total derivative element by element to a matrix"""
