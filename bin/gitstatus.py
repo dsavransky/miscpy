a69b34f10ffee7fdc06d42e2b31eca6ab12bfd48
Dmitry Savransky <dsavransky@gmail.com>
2021-06-18 10:11:42 -0400

    gitstatus update

--- bin/gitstatus.py
+++ bin/gitstatus.py
@@ -8,6 +8,12 @@ gitdirs = [os.path.join(basedir,d) for d in gitdirs]
 gitdirs += glob.glob(os.path.join(basedir,'gitrepos/*'))
 
 for d in gitdirs:
+    if "gitrepos" in d:
+        tmp = subprocess.run(["git", "remote", "-v"], cwd=d,
+                             capture_output=True).stdout.decode()
+        if "dsavransky" not in tmp:
+            continue
+
     res = subprocess.run(["git", "status"], cwd=d, capture_output=True).stdout.decode()
     if ("Your branch is up to date" in res) and ("nothing to commit" in res) and\
             ("working tree clean" in res):
